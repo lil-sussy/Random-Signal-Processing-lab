8149cda1cf4054bbd77d9fe46336bf493e28a8af
S U S S Y <46908682+lil-sussy@users.noreply.github.com>
2023-12-25 00:22:23 +0000

    Almost done with S2, will send email.

--- BiasedCrossCorr.m
+++ BiasedCrossCorr.m
@@ -1,15 +1,15 @@
-function [Cx,p] = BiasedCrossCorr(X,pmax)
+function [Cx, p] = BiasedCrossCorr(X, pmax)
     len = length(X);
     if nargin < 2
-        pmax = len-1;
+        pmax = len - 1;
     end
     
-    Cx = zeros(1,pmax + 1);
+    Cx = zeros(1, pmax + 1);
     p = 0:pmax;
     
-    for pvar=p
+    for pvar = p
         var1 = X(pvar + 1: len);
         var2 = conj(X(1:len-pvar));
-        Cx(pvar+1) = (1/len) .* sum(var1.*var2);
+        Cx(pvar + 1) = (1/len) .* sum(var1 .* var2);
     end
 end
